2a6192f6e0f626dd60ba105f2281dfca420fa79a
Ingo Wolf <me@ingowolf.au>
2026-01-11 21:30:53 +1100

    change hero

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import SearchBox from "@/components/searchbox";
 export default function Home() {
   return (
     <>
-      <div className="w-screen bg-blue-200 p-10 flex flex-col gap-4">
+      <div className="w-fit bg-blue-200 p-36 flex flex-col gap-4">
         <h1>Welcome to Service Table</h1>
         <SearchBox />
       </div>
