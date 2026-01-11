38e2af1b14c8beb0eed52274e52823fd972e9fb5
Ingo Wolf <me@ingowolf.au>
2026-01-11 17:47:28 +1100

    add home link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,8 +2,13 @@
 
 export default function Navbar() {
   return (
-    <div className="w-screen flex flex-row">
-      <h1 className="text-blue-400">Failsforce Service Desk</h1>
+    <div className="w-screen flex flex-row bg-blue-300">
+      {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+      <a href="/">
+        <h2 className="text-blue-500">
+          Failsforce Service Desk (click to go home)
+        </h2>
+      </a>
       <div className="grow"></div>
       <button onClick={() => (location.href = "/signup")}>Sign up</button>
       <button onClick={() => (location.href = "/login")}>Log in</button>
