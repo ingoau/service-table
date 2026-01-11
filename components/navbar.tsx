4895913472a43c614e5dab682ad5bfe2d4c39c9b
Ingo Wolf <me@ingowolf.au>
2026-01-11 17:43:28 +1100

    add login and sign up buttons

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,12 @@
+"use client";
+
 export default function Navbar() {
-  return <div></div>;
+  return (
+    <div className="w-screen flex flex-row">
+      <h1 className="text-blue-400">Failsforce Service Desk</h1>
+      <div className="grow"></div>
+      <button onClick={() => (location.href = "/signup")}>Sign up</button>
+      <button onClick={() => (location.href = "/login")}>Log in</button>
+    </div>
+  );
 }
