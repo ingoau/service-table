be57002af0a72d140ba578bccb36d3d8039b01e5
Ingo Wolf <me@ingowolf.au>
2026-01-11 18:11:47 +1100

    update some stutff

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -2,15 +2,18 @@
 
 export default function LogoutPage() {
   return (
-    <div>
-      <h1>Error</h1>
-      <p>you are not logged in so you cant log out</p>
+    <div className="mx-auto w-fit border border-red-500! p-4 m-4">
+      <h1 className="text-red-500">Error</h1>
+      <p>
+        you are not logged in so you cant log out, why are you pressing the log
+        out button
+      </p>
       <button
         onClick={() => {
           location.href = "/login";
         }}
       >
-        log in first
+        Log In First
       </button>
     </div>
   );
