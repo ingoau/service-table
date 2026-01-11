91aea48fa897965753635f65f161bc5c413e25c3
Ingo Wolf <me@ingowolf.au>
2026-01-12 09:39:52 +1100

    add best animations

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -2,6 +2,7 @@
 import { cn } from "@/lib/utils";
 import { useChat } from "@ai-sdk/react";
 import { lastAssistantMessageIsCompleteWithToolCalls } from "ai";
+import { AnimatePresence, motion } from "motion/react";
 import { useState } from "react";
 
 export default function Page() {
@@ -16,33 +17,44 @@ export default function Page() {
       {chat.messages.map((message) => {
         return (
           <div key={message.id} className="flex flex-col gap-2">
-            {message.parts.map((part, index) => {
-              if (part.type == "text")
-                return (
-                  <div
-                    key={index}
-                    className={cn(
-                      "p-4 w-fit rounded-full",
-                      message.role === "assistant"
-                        ? "bg-blue-200 max-w-4xl rounded-bl-none"
-                        : "bg-gray-200 ml-auto rounded-br-none",
-                    )}
-                  >
-                    {part.text}
-                  </div>
-                );
-              if (part.type == "tool-delegateToGork")
-                return (
-                  <div className="p-1 border w-fit rounded-full" key={index}>
-                    {part.state === "output-available"
-                      ? "Asked gork"
-                      : "Asking gork"}
-                    {' "'}
-                    {(part.input as { prompt: string })?.prompt || ""}
-                    {'"'}
-                  </div>
-                );
-            })}
+            <AnimatePresence>
+              {message.parts.map((part, index) => {
+                if (part.type == "text")
+                  return (
+                    <motion.div
+                      animate={{ y: 0 }}
+                      initial={{ y: 100 }}
+                      transition={{ duration: 1, ease: "linear" }}
+                      key={index}
+                      className={cn(
+                        "p-4 w-fit rounded-full",
+                        message.role === "assistant"
+                          ? "bg-blue-200 max-w-4xl rounded-bl-none"
+                          : "bg-gray-200 ml-auto rounded-br-none",
+                      )}
+                    >
+                      {part.text}
+                    </motion.div>
+                  );
+                if (part.type == "tool-delegateToGork")
+                  return (
+                    <motion.div
+                      animate={{ y: 0 }}
+                      initial={{ y: 100 }}
+                      transition={{ duration: 1, ease: "linear" }}
+                      className="p-1 border w-fit rounded-full"
+                      key={index}
+                    >
+                      {part.state === "output-available"
+                        ? "Asked gork"
+                        : "Asking gork"}
+                      {' "'}
+                      {(part.input as { prompt: string })?.prompt || ""}
+                      {'"'}
+                    </motion.div>
+                  );
+              })}
+            </AnimatePresence>
           </div>
         );
       })}
