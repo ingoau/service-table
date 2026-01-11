64f2c2fd3871755cd94ddf1dd05683bd074a7bc1
Ingo Wolf <me@ingowolf.au>
2026-01-11 21:23:00 +1100

    make ai button submit form

--- components/searchbox.tsx
+++ components/searchbox.tsx
@@ -2,17 +2,23 @@
 
 import { cn } from "@/lib/utils";
 import { AIButton } from "./ai-button";
+import { useRef } from "react";
 
 export default function SearchBox({ className }: { className?: string }) {
+  const formRef = useRef<HTMLFormElement>(null);
+
   return (
-    <form className={cn("flex", className)} action="/ai">
+    <form className={cn("flex", className)} action="/ai" ref={formRef}>
       <input
         type="text"
         name="query"
         defaultValue="Search with AI"
         className={"p-2"}
       />
-      <AIButton text="Search using AI" />
+      <AIButton
+        text="Search using AI"
+        onClick={() => formRef.current?.submit()}
+      />
     </form>
   );
 }
