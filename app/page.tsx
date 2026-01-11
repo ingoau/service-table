c92a1ae4642a084f7a10a8fd71beeae28d7cece9
Ingo Wolf <me@ingowolf.au>
2026-01-11 21:33:32 +1100

    change styles for hero

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import SearchBox from "@/components/searchbox";
 export default function Home() {
   return (
     <>
-      <div className="w-fit bg-blue-200 p-36 flex flex-col gap-4">
+      <div className="w-full bg-blue-200 p-36 flex flex-col gap-4 items-center">
         <h1>Welcome to Service Table</h1>
         <SearchBox />
         <a href="/createticket">Create a support ticket</a>
