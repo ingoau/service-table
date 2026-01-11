f0310990b9e76c6e98c6c558587ea9642bef012f
Ingo Wolf <me@ingowolf.au>
2026-01-12 09:00:52 +1100

    add testing interface

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+import { useChat } from "@ai-sdk/react";
+
 export default function Page() {
+  const chat = useChat();
+
   return (
     <div>
       <h1>AI</h1>
+      {JSON.stringify(chat.messages)}
+      <button
+        onClick={() => {
+          chat.sendMessage({ text: "e" });
+        }}
+      ></button>
     </div>
   );
 }
