9a53b739df7e590194cd2ed37b5537d3adf0caaa
Ingo Wolf <me@ingowolf.au>
2026-01-11 18:25:19 +1100

    delete homeapge contaent

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import AiButton from "@/components/ai-button";
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <>
-      <AiButton />
-      <AiButton extended={true} />
-    </>
-  );
+  return <></>;
 }
