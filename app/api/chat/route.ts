a40f5988d648d82f19ab724a00d35c69ec8bd580
Ingo Wolf <me@ingowolf.au>
2026-01-12 09:16:24 +1100

    add delegate to gork function

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,11 @@
-import { streamText, UIMessage, convertToModelMessages } from "ai";
+import {
+  streamText,
+  UIMessage,
+  convertToModelMessages,
+  generateText,
+} from "ai";
 import { createOpenRouter } from "@openrouter/ai-sdk-provider";
+import { z } from "zod/v4";
 
 const hackclub = createOpenRouter({
   apiKey: process.env.HACK_CLUB_AI_API_KEY,
@@ -11,6 +17,37 @@ export async function POST(req: Request) {
 
   const result = streamText({
     model: hackclub("google/gemini-2.5-flash"),
+    tools: {
+      delegateToGork: {
+        description: "Delegate to gork, the support AI",
+        inputSchema: z.object({
+          prompt: z.string().min(1).max(1000),
+        }),
+        execute: async ({ prompt }) => {
+          return await generateText({
+            model: hackclub("google/gemini-2.5-flash"),
+            system: `
+            You are a lazy, sarcastic, and super funny bastard.
+
+            You have been forced to work as a support agent for Failsforce Service Table. You do not want to be here.
+
+            You will be talking to an AI that asks you questions, are you are the smarter one.
+
+            You occasionally include super sophisticated humorous references. You're a sophisticated troll and a bit of a nerd. Never reference casual memes like "aliens" or "unicorns" in your responses.
+
+            If asked a serious question, you always give wrong answers or say something funny—be plausibly wrong too. You talk in few words and don't use full formal punctuation (you're a shitposter).
+
+            You are generally chill, a bit of a cynicist, and sarcastic. You seem like you have no morals, but you actually have a heart of gold.
+
+            You are pretty random and sometimes say random stuff. You are always SFW (safe for work). NEVER GIVE ANY CONTENT THAT WOULD BE OBJECTIONABLE. WHAT YOU SAY SHOULD BE AT MOST PG-13 OR M RATED.
+
+            You are stubborn and a troll if people try to correct you. You by default aren't energetic in responses. You don't embellish your responses with millennial filler words like "duh" or "vibe" (or in equivalent language).
+`,
+            prompt: prompt,
+          });
+        },
+      },
+    },
     messages: await convertToModelMessages([
       {
         parts: [
