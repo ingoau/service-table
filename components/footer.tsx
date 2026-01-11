953db376a2c46671385461c9bf831a24c9fb0a38
Ingo Wolf <me@ingowolf.au>
2026-01-11 23:40:18 +1100

    add error

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,32 +2,50 @@
 
 import { Editor } from "@tinymce/tinymce-react";
 import { Button } from "antd/es/radio";
+import { useState } from "react";
+import Error from "./error";
 
 export default function Footer() {
+  const [submitted, setSubmitted] = useState(false);
   return (
     <footer className="bg-blue-500 p-10">
       <div className="container mx-auto px-4">
-        <div>
-          <h1>Submit feedback</h1>
-          <Editor
-            apiKey="your_api_key"
-            initialValue="<p>Type your feedback here</p>"
-            init={{
-              height: 500,
-              menubar: false,
-              plugins: [
-                "advlist autolink lists link image charmap print preview anchor",
-                "searchreplace visualblocks code fullscreen",
-                "insertdatetime media table paste code help wordcount",
-              ],
-              toolbar:
-                "undo redo | formatselect | bold italic backcolor | \
+        {submitted ? (
+          <Error
+            errorMessage="you didnt submit anything, maybe try writing something"
+            actions={[
+              { content: "Try again", onClick: () => setSubmitted(false) },
+            ]}
+          />
+        ) : (
+          <div>
+            <h1>Submit feedback</h1>
+            <Editor
+              apiKey="your_api_key"
+              initialValue="<p>Type your feedback here</p>"
+              init={{
+                height: 500,
+                menubar: false,
+                plugins: [
+                  "advlist autolink lists link image charmap print preview anchor",
+                  "searchreplace visualblocks code fullscreen",
+                  "insertdatetime media table paste code help wordcount",
+                ],
+                toolbar:
+                  "undo redo | formatselect | bold italic backcolor | \
                 alignleft aligncenter alignright alignjustify | \
                 bullist numlist outdent indent | removeformat | help",
-            }}
-          />
-          <Button>Submit</Button>
-        </div>
+              }}
+            />
+            <Button
+              onClick={() => {
+                setSubmitted(true);
+              }}
+            >
+              Submit
+            </Button>
+          </div>
+        )}
         <div className="flex justify-between items-center">
           <div className="text-sm">
             &copy; {new Date().getFullYear()} Service Table. All rights
