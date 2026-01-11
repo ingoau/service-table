639b8311425f30e3575c445602126c50e666f0dd
Ingo Wolf <me@ingowolf.au>
2026-01-11 21:32:10 +1100

    add ai button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { AIButton } from "@/components/ai-button";
 import SearchBox from "@/components/searchbox";
 
 export default function Home() {
@@ -8,6 +9,11 @@ export default function Home() {
       <div className="w-fit bg-blue-200 p-36 flex flex-col gap-4">
         <h1>Welcome to Service Table</h1>
         <SearchBox />
+        <a href="/createticket">Create a support ticket</a>
+        or
+        <div>
+          <AIButton />
+        </div>
       </div>
     </>
   );
