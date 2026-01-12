846d52c68a1c978c54ac2ec6864611fddc00eff3
Ingo Wolf <me@ingowolf.au>
2026-01-12 11:54:04 +1100

    add logout page

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -1,15 +1,28 @@
 "use client";
 
-import { AIButton } from "@/components/ai-button";
 import Error from "@/components/error";
+import { authClient } from "@/lib/auth-client";
 
 export default function LogoutPage() {
+  const session = authClient.useSession();
+  if (!session.data) {
+    return (
+      <Error
+        errorMessage="you are not logged in so you cant log out, why are you pressing the log out button? do you want to log in? idk anymore lol"
+        actions={[
+          {
+            content: "Log In First",
+            onClick: () => (location.href = "/login"),
+          },
+        ]}
+      />
+    );
+  }
   return (
-    <Error
-      errorMessage="you are not logged in so you cant log out, why are you pressing the log out button? do you want to log in? idk anymore lol"
-      actions={[
-        { content: "Log In First", onClick: () => (location.href = "/login") },
-      ]}
-    />
+    <>
+      <button onClick={() => authClient.signOut()} className="m-4 p-4">
+        Log out
+      </button>
+    </>
   );
 }
