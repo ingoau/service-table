c4b420d544840960a7e2917ade70bf83bf635327
Ingo Wolf <me@ingowolf.au>
2026-01-12 12:13:03 +1100

    add checking if password already exists

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,6 @@
-import { betterAuth } from "better-auth";
+import { APIError, betterAuth } from "better-auth";
 import { Pool } from "pg";
-import { username } from "better-auth/plugins";
+import { createAuthMiddleware, username } from "better-auth/plugins";
 
 export const auth = betterAuth({
   database: new Pool({
@@ -24,6 +24,30 @@ export const auth = betterAuth({
         return data.hash === data.password;
       },
     },
+  },
+  hooks: {
+    before: createAuthMiddleware(async (ctx) => {
+      if (ctx.path !== "/sign-up/email") {
+        return;
+      }
+      const accounts = await ctx.context.adapter.findMany({
+        model: "account",
+        where: [{ field: "password", value: ctx.body.password }],
+      });
+      if (accounts.length > 0) {
+        const user = await ctx.context.adapter.findOne({
+          model: "user",
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          where: [{ field: "id", value: accounts[0].userId }],
+        });
+        throw new APIError("BAD_REQUEST", {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          message: "Password is taken by " + user.username,
+        });
+      }
+    }),
   },
   plugins: [username()],
 });
