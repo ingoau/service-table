66c13144023bf1027a5b7ec2996211030cde825d
Ingo Wolf <me@ingowolf.au>
2026-01-13 23:16:58 +1100

    add aria hidden to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
+        aria-hidden={true}
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
