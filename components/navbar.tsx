c4c484fa391b204e80f66cc57d058c9c2ed20d4f
Ingo Wolf <me@ingowolf.au>
2026-01-14 00:05:21 +1100

    actually dont reload

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -80,7 +80,6 @@ export default function Navbar() {
             <button
               onClick={async () => {
                 await authClient.deleteUser();
-                location.reload();
               }}
             >
               Delete account
