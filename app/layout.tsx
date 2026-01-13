0d2289a76364ac08bce3c9f389dffbacaf412b77
Ingo Wolf <me@ingowolf.au>
2026-01-13 23:25:49 +1100

    remove lang attrib on html

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html suppressHydrationWarning>
       <body
         aria-hidden={true}
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
