c725002498a56c032ae25f2ab3f7e296205d2d37
Ingo Wolf <me@ingowolf.au>
2026-01-14 00:42:24 +1100

    delete proxy

--- proxy.ts
+++
@@ -1,9 +0,0 @@
-// middleware.ts
-import { NextResponse } from "next/server";
-
-const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
-
-export async function proxy() {
-  await sleep(300);
-  return NextResponse.next();
-}
