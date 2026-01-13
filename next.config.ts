e3673578827e84ba0a44b97965f3c5d2bfd70c3f
Ingo Wolf <me@ingowolf.au>
2026-01-13 23:11:43 +1100

    optimise

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compress: false,
+  compiler: {
+    styledComponents: false,
+    emotion: false,
+  },
+  images: {
+    unoptimized: true,
+  },
+  productionBrowserSourceMaps: true,
 };
 
 export default nextConfig;
