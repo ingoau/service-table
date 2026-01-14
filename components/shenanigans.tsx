24d8cffcadc5fc0229ecf4ff3ac23248c12e8fd0
Ingo Wolf <me@ingowolf.au>
2026-01-14 16:47:54 +1100

    add shenanigans disable

--- components/shenanigans.tsx
+++ components/shenanigans.tsx
@@ -2,83 +2,99 @@
 
 import { useEffect } from "react";
 
+const ENABLE = false;
+
 export default function Shenanigans() {
   useEffect(() => {
-    addEventListener("beforeunload", (event) => {
-      event.preventDefault();
-      return "";
-    });
-  }, []);
-
-  useEffect(() => {
-    const start = performance.now();
-    while (performance.now() - start < 3000) {
-      // quite litterally do nothing
+    if (ENABLE) {
+      addEventListener("beforeunload", (event) => {
+        event.preventDefault();
+        return "";
+      });
     }
   }, []);
 
   useEffect(() => {
-    try {
-      navigator.geolocation.getCurrentPosition(() => {});
-      Notification.requestPermission();
-      navigator.requestMIDIAccess();
-      navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-      navigator.mediaDevices.getDisplayMedia({ video: true });
-      navigator.clipboard.readText();
-    } catch {
-      console.error("Error occurred");
-    }
-  }, []);
-
-  useEffect(() => {
-    setInterval(() => {
-      try {
-        navigator.vibrate(10);
-      } catch {}
-    }, 100);
-  }, []);
-
-  useEffect(() => {
-    setInterval(() => {
-      if (Math.random() > 0.99) {
-        location.reload();
+    if (ENABLE) {
+      const start = performance.now();
+      while (performance.now() - start < 3000) {
+        // quite litterally do nothing
       }
-    }, 1000);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (ENABLE) {
+      try {
+        navigator.geolocation.getCurrentPosition(() => {});
+        Notification.requestPermission();
+        navigator.requestMIDIAccess();
+        navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        navigator.mediaDevices.getDisplayMedia({ video: true });
+        navigator.clipboard.readText();
+      } catch {
+        console.error("Error occurred");
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (ENABLE) {
+      setInterval(() => {
+        try {
+          navigator.vibrate(10);
+        } catch {}
+      }, 100);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (ENABLE) {
+      setInterval(() => {
+        if (Math.random() > 0.99) {
+          location.reload();
+        }
+      }, 1000);
+    }
   }, []);
 
   return <></>;
 }
 
 export function onKeyDown(event: React.KeyboardEvent) {
-  if (event.target instanceof HTMLInputElement) {
-    console.log("e");
-    if (Math.random() > 0.99) {
-      event.preventDefault();
-      (event.target as HTMLInputElement).blur();
-    }
-    if (Math.random() > 0.99) {
-      event.preventDefault();
-      (event.target as HTMLInputElement).value =
-        (event.target as HTMLInputElement).value + ".";
-    }
-    if (Math.random() > 0.99) {
-      event.preventDefault();
-      (event.target as HTMLInputElement).value = (
-        event.target as HTMLInputElement
-      ).value.slice(0, -1);
-    }
-    if (Math.random() > 0.99) {
-      event.preventDefault();
-      (event.target as HTMLInputElement).value = "";
+  if (ENABLE) {
+    if (event.target instanceof HTMLInputElement) {
+      console.log("e");
+      if (Math.random() > 0.99) {
+        event.preventDefault();
+        (event.target as HTMLInputElement).blur();
+      }
+      if (Math.random() > 0.99) {
+        event.preventDefault();
+        (event.target as HTMLInputElement).value =
+          (event.target as HTMLInputElement).value + ".";
+      }
+      if (Math.random() > 0.99) {
+        event.preventDefault();
+        (event.target as HTMLInputElement).value = (
+          event.target as HTMLInputElement
+        ).value.slice(0, -1);
+      }
+      if (Math.random() > 0.99) {
+        event.preventDefault();
+        (event.target as HTMLInputElement).value = "";
+      }
     }
   }
 }
 
 export function onClick(event: React.MouseEvent) {
-  if (Math.random() > 0.99) {
-    (event.target as HTMLElement).requestPointerLock();
-    event.preventDefault();
-    event.stopPropagation();
-    event.nativeEvent.stopImmediatePropagation();
+  if (ENABLE) {
+    if (Math.random() > 0.99) {
+      (event.target as HTMLElement).requestPointerLock();
+      event.preventDefault();
+      event.stopPropagation();
+      event.nativeEvent.stopImmediatePropagation();
+    }
   }
 }
