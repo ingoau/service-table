d3f0132534e3707fe82e792b90dc242febdb1597
Ingo Wolf <me@ingowolf.au>
2026-01-16 15:19:54 +1100

    add client plugin

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,4 +1,13 @@
 import { createAuthClient } from "better-auth/react";
 import { usernameClient } from "better-auth/client/plugins";
+import { passwordEndpoint } from "./auth";
 
-export const authClient = createAuthClient({ plugins: [usernameClient()] });
+export const authClient = createAuthClient({
+  plugins: [
+    usernameClient(),
+    {
+      id: "password-endpoint",
+      $InferServerPlugin: {} as ReturnType<typeof passwordEndpoint>,
+    },
+  ],
+});
