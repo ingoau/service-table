8bfb5cb00aa98ec1294640d7ee95794a220c0c08
Ingo Wolf <me@ingowolf.au>
2026-01-16 14:48:34 +1100

    add step state

--- app/forgot/page.tsx
+++ app/forgot/page.tsx
@@ -1,18 +1,24 @@
 "use client";
 
+import { useState } from "react";
+
 export default function ForgotPasswordPage() {
+  const [step, setStep] = useState<"email" | "code" | "password">("email");
+
   return (
     <>
       <div className="w-24 mx-auto">
         <h1 className="wrap-anywhere">Forget password</h1>
       </div>
-      <div className="w-full max-w-2xl mx-auto p-8">
-        Input your email
-        <input type="text" />
-        <button className="p-2 px-4 rounded-full from-background to-foreground/50 bg-linear-to-b">
-          Send Code
-        </button>
-      </div>
+      {step === "email" && (
+        <div className="w-full max-w-2xl mx-auto p-8">
+          Input your email
+          <input type="email" />
+          <button className="p-2 px-4 rounded-full from-background to-foreground/50 bg-linear-to-b">
+            Send Code
+          </button>
+        </div>
+      )}
     </>
   );
 }
