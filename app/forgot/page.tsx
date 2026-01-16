3625a3983adb72680e3c38f952e88ee54e69faed
Ingo Wolf <me@ingowolf.au>
2026-01-16 14:50:33 +1100

    add email state

--- app/forgot/page.tsx
+++ app/forgot/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 export default function ForgotPasswordPage() {
   const [step, setStep] = useState<"email" | "code" | "password">("email");
+  const [email, setEmail] = useState<string>("");
   const [code, setCode] = useState<string>("");
 
   return (
@@ -14,8 +15,21 @@ export default function ForgotPasswordPage() {
       {step === "email" && (
         <div className="w-full max-w-2xl mx-auto p-8">
           Input your email in the following field:
-          <input type="email" />
-          <button className="p-2 px-4 rounded-full from-background to-foreground/50 bg-linear-to-b">
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button
+            className="p-2 px-4 rounded-full from-background to-foreground/50 bg-linear-to-b"
+            onClick={() => {
+              const randomCode = Math.floor(
+                100000 + Math.random() * 900000,
+              ).toString();
+              setCode(randomCode);
+              setStep("code");
+            }}
+          >
             Send Code
           </button>
         </div>
