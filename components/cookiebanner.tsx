996a3ea088fa31d6fa095127d92e0db0c18ef138
Ingo Wolf <me@ingowolf.au>
2026-01-17 20:37:52 +1100

    add state

--- components/cookiebanner.tsx
+++ components/cookiebanner.tsx
@@ -1,17 +1,26 @@
 "use client";
 
 import { motion } from "motion/react";
+import { useState } from "react";
 
 export default function CookieBanner() {
+  const [isManagingPreferences, setIsManagingPreferences] = useState(false);
   return (
     <motion.div className="bg-red-500 p-10">
       <h1>We use cookies to improve your experience!</h1>
       Your privacy is our priority, so it is your choice to agree or decline.
       <br />
-      <button className="p-10 text-4xl">Agree</button>
-      <button className="text-xs bg-transparent border-0 outline-0">
-        Manage preferences
-      </button>
+      {!isManagingPreferences && (
+        <>
+          <button className="p-10 text-4xl">Agree</button>
+          <button
+            className="text-xs bg-transparent border-0 outline-0"
+            onClick={() => setIsManagingPreferences(true)}
+          >
+            Manage preferences
+          </button>
+        </>
+      )}
     </motion.div>
   );
 }
