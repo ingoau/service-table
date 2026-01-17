381f653f489b903e4fe76f0c023c6dd93f884f54
Ingo Wolf <me@ingowolf.au>
2026-01-17 20:24:59 +1100

    add padding to laoder

--- components/loading.tsx
+++ components/loading.tsx
@@ -4,9 +4,11 @@ import { Loader2 } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="animate-spin origin-left">
-      <Loader2 />
-      Loading...
+    <div className="p-4">
+      <div className="animate-spin origin-left">
+        <Loader2 />
+        Loading...
+      </div>
     </div>
   );
 }
